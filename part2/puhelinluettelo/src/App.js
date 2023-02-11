38c7bcb029b74ece0e63a5a19c80ce0c1b16f565
jyddys1 <justus.sydanmaa@hotmail.com>
2023-02-11 11:57:52 +0200

    Moved data into different json file

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,18 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from "./components/Filter"
 import PersonForm from "./components/PersonForm"
 import Persons from "./components/Persons"
+import './App.css';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -26,7 +31,6 @@ const App = () => {
     alert(`${newName} is already added to phonebook`)
     return
    }
-
 
     setPersons([...persons, personObj])
     setNewName('')
@@ -51,7 +55,7 @@ const App = () => {
   );
 
   return (
-    <div>
+    <div className='container'>
       <h2>Phonebook</h2>
       <Filter 
         search={search}
